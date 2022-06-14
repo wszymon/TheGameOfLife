c5a9d8230ca191addb447f41af43f88b722ef29b
wszymon <ja@DESKTOP-RN0UR9P>
2022-06-14 20:49:25 +0200

    Add Test class

---
+++ src/game/BoardTest.java
@@ -0,0 +1,12 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+    @Test
+    void isThereRightAmountOfNeighbors(){
+
+    }
+}
